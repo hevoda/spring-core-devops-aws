9fcbab0f6f8b9bb5a5346c4b71c414c13955fc49
Hervé EVODA <herveevoda@gmail.com>
2024-05-06 15:44:18 +0200

    Spring Boot profile Properties 2.

---
+++ src/main/java/guru/springframework/bootstrap/profilesysout/NonProfileBean.java
@@ -0,0 +1,16 @@
+package guru.springframework.bootstrap.profilesysout;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NonProfileBean {
+
+    @Autowired
+    public NonProfileBean(@Value("${guru.springframework.profile.message}")  String msg){
+
+        System.out.println("**********"+  msg + "**********");
+
+    }
+}
