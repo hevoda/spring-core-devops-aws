86a74f36c7d82599b972b276ca73bf4db88947f0
Hervé EVODA <herveevoda@gmail.com>
2024-05-29 22:24:35 +0200

    Adding and configuring  qa2 data source.

--- src/main/java/guru/springframework/bootstrap/DevOpsBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/DevOpsBootstrap.java
@@ -42,7 +42,7 @@ public class DevOpsBootstrap implements ApplicationListener<ContextRefreshedEven
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 
-        if(IteratorUtils.toList(authorRepository.findAll().iterator()).size() == 0){
+       if(IteratorUtils.toList(authorRepository.findAll().iterator()).size() == 0){
         Author jt = new Author();
         jt.setFirstName("John");
         jt.setLastName("Thompson");
